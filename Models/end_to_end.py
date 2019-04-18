50b3275cf7df1951ada583abd058215b2759f067
Victor <vreyesos@uwaterloo.ca>
2019-04-18 17:54:56 -0400

    New log and the ability to set parameters through an ini file

--- Models/end_to_end.py
+++ Models/end_to_end.py
@@ -6,7 +6,7 @@ from keras.regularizers import l1_l2
 
 from .neural_layers import residual_block, bilinear_resize, expand_dims
 
-def create_network(input_shape, prior, L1=0, L2=0, dropout=0, KERNEL_NUM = 16):
+def create_network(input_shape, prior, L1=0, L2=0, dropout=0, KERNEL_NUM = 64):
     inp = Input(shape=input_shape)
     x = Conv2D(KERNEL_NUM, 7, padding='same', strides=2, kernel_regularizer=l1_l2(l1=L1, l2=L2))(inp)
     x = BatchNormalization()(x)
@@ -19,16 +19,16 @@ def create_network(input_shape, prior, L1=0, L2=0, dropout=0, KERNEL_NUM = 16):
     x = Dropout(dropout)(x)
 
     # First Residual Block
-    x = residual_block(x, bottleneck_kernels=KERNEL_NUM,
-                               out_kernels=4*KERNEL_NUM,
+    x = residual_block(x, bottleneck_kernels=KERNEL_NUM//4,
+                               out_kernels=KERNEL_NUM,
                                kernel_size=3,
                                identity=False,
                                L1=L1,
                                L2=L2)
     x = Dropout(dropout)(x)
     for i in range(2):
-        x = residual_block(x, bottleneck_kernels=KERNEL_NUM,
-                                    out_kernels=4*KERNEL_NUM,
+        x = residual_block(x, bottleneck_kernels=KERNEL_NUM//4,
+                                    out_kernels=KERNEL_NUM,
                                     kernel_size=3,
                                     identity=True,
                                     L1=L1,
@@ -42,16 +42,16 @@ def create_network(input_shape, prior, L1=0, L2=0, dropout=0, KERNEL_NUM = 16):
     x = Dropout(dropout)(x)
 
     # Second Residual Block
-    x = residual_block(x, bottleneck_kernels=2*KERNEL_NUM,
-                               out_kernels=8*KERNEL_NUM,
+    x = residual_block(x, bottleneck_kernels=KERNEL_NUM//2,
+                               out_kernels=2*KERNEL_NUM,
                                kernel_size=3,
                                identity=False,
                                L1=L1,
                                L2=L2)
     x = Dropout(dropout)(x)
     for i in range(3):
-        x = residual_block(x, bottleneck_kernels=2*KERNEL_NUM,
-                                    out_kernels=8*KERNEL_NUM,
+        x = residual_block(x, bottleneck_kernels=KERNEL_NUM//2,
+                                    out_kernels=2*KERNEL_NUM,
                                     kernel_size=3,
                                     identity=True,
                                     L1=L1,
@@ -59,22 +59,22 @@ def create_network(input_shape, prior, L1=0, L2=0, dropout=0, KERNEL_NUM = 16):
         x = Dropout(dropout)(x)
 
     # Downsampling
-    x = Conv2D(8*KERNEL_NUM, 3, padding='same', strides=2, kernel_regularizer=l1_l2(l1=L1, l2=L2))(x)
+    x = Conv2D(2*KERNEL_NUM, 3, padding='same', strides=2, kernel_regularizer=l1_l2(l1=L1, l2=L2))(x)
     x = BatchNormalization()(x)
     x = LeakyReLU()(x)
     x = Dropout(dropout)(x)
 
     # Third Residual Block
-    x = residual_block(x, bottleneck_kernels=4*KERNEL_NUM,
-                               out_kernels=16*KERNEL_NUM,
+    x = residual_block(x, bottleneck_kernels=KERNEL_NUM,
+                               out_kernels=4*KERNEL_NUM,
                                kernel_size=3,
                                identity=False,
                                L1=L1,
                                L2=L2)
     x = Dropout(dropout)(x)
     for i in range(5):
-        x = residual_block(x, bottleneck_kernels=4*KERNEL_NUM,
-                                    out_kernels=16*KERNEL_NUM,
+        x = residual_block(x, bottleneck_kernels=KERNEL_NUM,
+                                    out_kernels=4*KERNEL_NUM,
                                     kernel_size=3,
                                     identity=True,
                                     L1=L1,
@@ -88,8 +88,8 @@ def create_network(input_shape, prior, L1=0, L2=0, dropout=0, KERNEL_NUM = 16):
     x = concatenate([x,repeat], axis=3)
 
     # Trainable residual 3D blocks
-    x = residual_block(x, bottleneck_kernels=8*KERNEL_NUM,
-                                out_kernels=32*KERNEL_NUM,
+    x = residual_block(x, bottleneck_kernels=2*KERNEL_NUM,
+                                out_kernels=8*KERNEL_NUM,
                                 kernel_size=3,
                                 identity=False,
                                 conv=Conv3D,
@@ -98,8 +98,8 @@ def create_network(input_shape, prior, L1=0, L2=0, dropout=0, KERNEL_NUM = 16):
     x = Dropout(dropout)(x)
 
     for i in range(2): # Originally 2
-        x = residual_block(x, bottleneck_kernels=8*KERNEL_NUM,
-                                   out_kernels=32*KERNEL_NUM,
+        x = residual_block(x, bottleneck_kernels=2*KERNEL_NUM,
+                                   out_kernels=8*KERNEL_NUM,
                                    kernel_size=3,
                                    identity=True,
                                    conv=Conv3D,
--- training.py
+++ training.py
@@ -4,13 +4,13 @@ from keras.callbacks import CSVLogger, ModelCheckpoint, ReduceLROnPlateau
 import tensorflow as tf
 import numpy as np
 
-def prepare_model(create_network, input_shape, loss, optimizer, metrics, prior, GPUs=1, L1=0, L2=0, dropout=0):
+def prepare_model(create_network, input_shape, loss, optimizer, metrics, prior, GPUs=1, L1=0, L2=0, dropout=0, kernel_num=64):
     if GPUs==1:
-        template_model = create_network(input_shape, prior, L1=L1, L2=L2, dropout=dropout)
+        template_model = create_network(input_shape, prior, L1=L1, L2=L2, dropout=dropout, KERNEL_NUM=kernel_num)
         model = template_model
     elif GPUs>1:
         with tf.device('/cpu:0'):
-            template_model = create_network(input_shape, prior, L1=L1, L2=L2, dropout=dropout)
+            template_model = create_network(input_shape, prior, L1=L1, L2=L2, dropout=dropout, KERNEL_NUM=kernel_num)
             model = multi_gpu_model(template_model, gpus=GPUs)
     else:
         raise ValueError('GPUs needs to be an integer greater than or equal to 1, not {}'.format(GPUs))
@@ -40,53 +40,123 @@ def data_generator(batch_size, imgs, pos_cubes, neg_cubes):
 
             yield batch_images, batch_cubes
 
-def load_data():
+def load_data(train_set, val_set):
+    print('\tLoading Targets')
+    train_pos_cubes = np.load('Data/for_training/training_positive_cubes.npy').astype(np.int32)[:, 1:]
+    train_neg_cubes = np.load('Data/for_training/training_negative_cubes.npy').astype(np.int32)[:, 1:]
+
+    val_pos_cubes = np.load('Data/for_training/validation_positive_cubes.npy').astype(np.int32)[:, 1:]
+    val_neg_cubes = np.load('Data/for_training/validation_negative_cubes.npy').astype(np.int32)[:, 1:]
+
     print('\tLoading Images')
     #train_images = np.zeros([31062,224,224,3], dtype=np.int8)
-    train_images = np.load('Data/for_training/small_image_set.npy')
-    val_images = np.load('Data/for_training/validation_image_data.npy')[571:]
+    if train_set=='small':
+        train_images = np.load('Data/for_training/small_image_set.npy')
+        train_pos_cubes = train_pos_cubes[:512]
+        train_neg_cubes = train_neg_cubes[:512]
+    elif train_set=='full':
+        train_images = np.load('Data/for_training/training_image_data.npy')
+    else:
+        raise ValueError('{} is not a valid training_set'.format(train_set))
+
+    val_images = np.load('Data/for_training/validation_image_data.npy')
+
+    if val_set=='random':
+        val_images = val_images[571:]
+        val_pos_cubes = val_pos_cubes[571:]
+        val_neg_cubes = val_neg_cubes[571:]
+        total_num = 1000-571
+    elif val_set=='object':
+        val_images = val_images[:571]
+        val_pos_cubes = val_pos_cubes[:571]
+        val_neg_cubes = val_neg_cubes[:571]
+        total_num = 571
+    elif val_set=='full':
+        total_num = 1000
+    else:
+        raise ValueError('{} is not a valid validation_set'.format(val_set))
     val_images = np.concatenate([val_images,val_images],axis=0)
 
     print('\tPreprocessing Images')
     train_images = resnet50.preprocess_input(train_images)
     val_images = resnet50.preprocess_input(val_images)
 
-    print('\tLoading Targets')
-    train_pos_cubes = np.load('Data/for_training/training_positive_cubes.npy').astype(np.int32)[:512, 1:]
-    train_neg_cubes = np.load('Data/for_training/training_negative_cubes.npy').astype(np.int32)[:512, 1:]
-
-    val_pos_cubes = np.load('Data/for_training/validation_positive_cubes.npy').astype(np.int32)[571:, 1:]
-    val_neg_cubes = np.load('Data/for_training/validation_negative_cubes.npy').astype(np.int32)[571:, 1:]
-
-    validation_cubes = np.zeros([(1000-571)*2, 8,8,8])
-    for i in range(1000-571):
+    print('\tProcessing Validation Set')
+    validation_cubes = np.zeros([total_num*2, 8,8,8])
+    for i in range(total_num):
         pz,py,px = val_pos_cubes[i]
         validation_cubes[i, pz,py,px] = 1
         nz,ny,nx = val_neg_cubes[i]
-        validation_cubes[i+(1000-571), nz,ny,nx] = 0.2
+        validation_cubes[i+total_num, nz,ny,nx] = 0.2
 
     return (train_images, val_images), (train_pos_cubes, train_neg_cubes), validation_cubes
 
 if __name__ == '__main__':
-    from Models.resnet_style import create_network
+    import configparser
+    config = configparser.ConfigParser()
+    config.read('training_config.ini')
 
-    # Hyperparameters
-    BATCH_SIZE = 64
-    L1_REGULARIZER = 0
-    L2_REGULARIZER = 0
-    LEARNING_RATE = 1e-5
-    MOMENTUM = 0
-    DROPOUT_RATE = 0
+    # Architecture
+    arch = config['architecture']['architecture']
+    kernel_num = config['architecture'].getint('kernel_num')
+    if arch == 'resnet_style':
+        from Models.resnet_style import create_network
+    elif arch == 'end_to_end':
+        from Models.end_to_end import create_network
+    else:
+        raise ValueError('{} is not a valid architecture'.format(arch))
+
+    # Optimizer
+    opt_name      = config['optimizer']['optimizer']
+    learning_rate = config['optimizer'].getfloat('learning_rate')
+    clipnorm      = config['optimizer'].getfloat('clipnorm')
+    momentum      = config['optimizer'].getfloat('momentum')
+    if opt_name == 'adam':
+        from keras.optimizers import Adam
+        opt = Adam(lr=learning_rate, clipnorm=clipnorm)
+    elif opt_name == 'sgd':
+        from keras.optimizers import SGD
+        opt = SGD(lr=learning_rate, clipnorm=clipnorm, momentum=momentum)
+    else:
+        raise ValueError('{} is not a valid optimizer'.format(opt_name))
+
+    # Regularizer
+    L1_REGULARIZER = config['regularizer'].getfloat('l1_penalty')
+    L2_REGULARIZER = config['regularizer'].getfloat('l2_penalty')
+    DROPOUT_RATE   = config['regularizer'].getfloat('dropout_rate')
 
     # Callbacks
-    csv_logger = CSVLogger('training.log')
-    model_checkpoint = ModelCheckpoint('checkpoints/best_model.h5', save_best_only=True)
-    reduce_lr_on_plateau = ReduceLROnPlateau(monitor='val_loss', factor=0.1, patience=10, min_delta=1e-4)
+    callbacks = []
+    if config['callbacks'].getboolean('csv_logger'):
+        csv_fname  = config['callbacks']['csv_fname']
+        csv_logger = CSVLogger(csv_fname)
+        callbacks.append(csv_logger)
+
+    if config['callbacks'].getboolean('model_checkpoint'):
+        model_fname = config['callbacks']['model_fname']
+        save_best   = config['callbacks'].getboolean('model_save_best')
+        model_checkpoint = ModelCheckpoint(model_fname, save_best_only=save_best)
+        callbacks.append(model_checkpoint)
+
+    if config['callbacks'].getboolean('reduce_lr_on_plateau'):
+        monitor   = config['callbacks']['reduce_lr_monitor']
+        factor    = config['callbacks']['reduce_lr_factor']
+        patience  = config['callbacks'].getfloat('reduce_lr_patience')
+        min_delta = config['callbacks'].getfloat('reduce_lr_min_delta')
+        reduce_lr_on_plateau = ReduceLROnPlateau(monitor=monitor,
+                                                 factor=factor,
+                                                 patience=patience,
+                                                 min_delta=min_delta)
+        callbacks.append(reduce_lr_on_plateau)
+
+    # Hyperparameters
+    BATCH_SIZE = config['training'].getint('batch_size')
+    val_set    = config['training']['validation_set']
+    train_set  = config['training']['training_set']
+
 
     print('Building Network')
-    from keras.optimizers import SGD, Adam
     from Models.loss_and_metrics import single_accuracy, all_way_binary_cross_entropy
-    opt = Adam(lr=LEARNING_RATE, clipnorm=1)
 
     (template, model) = prepare_model(create_network, input_shape=[224,224,3],
                                                       loss=all_way_binary_cross_entropy,
@@ -96,10 +166,15 @@ if __name__ == '__main__':
                                                       L1=L1_REGULARIZER,
                                                       L2=L2_REGULARIZER,
                                                       dropout=DROPOUT_RATE,
-                                                      prior=np.load('Data/for_training/prior_pos.npy'))
+                                                      prior=np.load('Data/for_training/prior_pos.npy'),
+                                                      kernel_num=kernel_num)
 
     print('Loading Data')
-    (train_images, val_images), (train_pos_cubes, train_neg_cubes), validation_cubes = load_data()
+    train_set = config['training']['training_set']
+    val_set   = config['training']['validation_set']
+    images, cubes, validation_cubes = load_data(train_set, val_set)
+    train_images, val_images = images
+    train_pos_cubes, train_neg_cubes = cubes
 
     print('Configuring Generator')
     datagen = data_generator(BATCH_SIZE, train_images, train_pos_cubes, train_neg_cubes)
